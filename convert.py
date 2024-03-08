1a1a893a88908bc67cf6fba774060059733c5ee9
Shailendra Singh <viperfury23@gmail.com>
2024-03-09 03:24:35 +0530

    major changes

--- convert.py
+++ convert.py
@@ -2,52 +2,49 @@ import re
 import sys
 from src.build_nfa import transform
 from src.visualize_nfa import visualize
-
+from src.transitiontable_nfa import table
 
 # global constant regex
-RANGES = "\[.-.\]|\[.-..-.\]"
-LETTERS = "[A-Za-z]"
-DIGITS = "[0-9]"
-SPECIALS = "\(|\)|\||\+|\*"
-
+RANGE = "\[.-.\]|\[.-..-.\]"
+LETTER = "[A-Za-z]"
+DIGIT = "[0-9]"
+SPECIAL = "\(|\)|\||\+|\*"
 
 def validate(regex):
-    # check whether input expression contains ranges or not
-    if re.search(RANGES, regex):
-        # exit in case of ranges (ranges not allowed)
-        raise Exception("Ranges are NOT allowed !")
+    # check whether input expression contains RANGEZ or not
+    if re.search(RANGE, regex):
+        # exit in case of RANGE (RANGE not allowed)
+        raise Exception("RANGES are NOT allowed !")
     # check validity of each character (escapable special characters)
     idx = 0
     while idx < len(regex):
         # check if the character is not a letter nor a number nor a special
-        if not (re.search(LETTERS, regex[idx]) or re.search(DIGITS, regex[idx]) \
-            or re.search(SPECIALS, regex[idx])):
+        if not (re.search(LETTER, regex[idx]) or re.search(DIGIT, regex[idx]) \
+            or re.search(SPECIAL, regex[idx])):
             # other special characters must be preceded with '\'
             if regex[idx] == "\\":
                 idx += 1
             else:
                 raise Exception("Use \ to escape special characters !")
         idx += 1
-    # try compiling input expression
-    # check for general regex syntax and brackets match
+
     try:
         re.compile(regex)
     except re.error:
         raise Exception("Input Regex is incorrect !")
 
-
+# the main function calling all other worker functions
 def convert(regex):
-    # validate input regex
     validate(regex)
-    # transform regex to nfa
+
     nfa = transform(regex)
-    # visualize nfa
+
     visualize(nfa)
 
+    table(nfa)
 
 if __name__ == "__main__":
-    # initialize regex-to-nfa conversion
     if len(sys.argv) == 2:
         convert(sys.argv[1])
     else:
-        raise Exception("Enter Target Regex !")
+        raise Exception("No regular recieved !")
--- src/build_nfa.py
+++ src/build_nfa.py
@@ -1,276 +1,187 @@
 import collections
 import json
 
-
 def solveBracket(regex, end_state, states):
-    """
-        This function used to solve the regex inside a bracket and save the new states to the current available states
-    """
+    # Initialize variables for the bracket expression
+    b_start = b_prev = b_char = b_end = end_state + 1
+    states[b_end] = {"isFinalState": False}
 
-    # initialization for the new states to be created must start with the last created state + 1
-    b_start = end_state + 1
-    b_prev = end_state + 1
-    b_char = end_state + 1
-    b_end = end_state + 1
-    # create new state to indicate we are working with bracket regex
-    states.update( { b_end : { "isTerminatingState": False } })
-    # start looping over the regex inside the bracket to transform it
     index = 0
-    while(index < len(regex)):
-        # in case of oring operation found 
-        if regex[index] == '|' or regex[index] == '+':
-            # solve it using oring function (function description below) 
-            index, prev, start,end = oring(index+1, regex, states , b_end)
-            # create new 2 states to connect the oring branches
-            states.update( { end+1 : { "isTerminatingState": False, "ε" : b_prev , "ε " : prev } })
-            states.update( { end+2 : { "isTerminatingState": False} })
-            states[end].update({"ε" : end+2})
-            states[b_end].update({"ε" : end+2})
-            #update the indices used to loop in states
-            b_char = end + 1
-            b_end = end +2
-            b_start = b_end
-            b_prev = end + 1
-        # in case of new bracket found
+    # Iterate through the characters in the bracket expression
+    while index < len(regex):
+        if regex[index] in {'|', '+'}:
+            # Handle OR operations within the bracket
+            index, prev, start, end = ORing(index + 1, regex, states, b_end)
+            # Update states based on the OR operation
+            states.update({end + 1: {"isFinalState": False, "ε": b_prev, "ε ": prev},
+                           end + 2: {"isFinalState": False}})
+            states[end].update({"ε": end + 2})
+            states[b_end].update({"ε": end + 2})
+            b_char, b_end, b_start, b_prev = end + 1, end + 2, end + 2, end + 1
         elif regex[index] == '(':
-            # get the regex inside that bracket
-            open_brackets = 1
-            closed_brackets = 0
-            sub_regex = ""
-            j = index+1
-            while(j<len(regex)):
+            # Handle nested brackets
+            j = index + 1
+            open_brackets, closed_brackets, sub_regex = 1, 0, ""
+            while j < len(regex):
                 if regex[j] == '(':
-                    open_brackets +=1
+                    open_brackets += 1
                 elif regex[j] == ')':
-                    closed_brackets +=1
-                if (open_brackets == closed_brackets):
+                    closed_brackets += 1
+                if open_brackets == closed_brackets:
                     break
                 sub_regex += regex[j]
                 j += 1
-            # then solve it using this function recursively
-            prev , start, end = solveBracket(sub_regex, b_end, states) 
-            # connect the new bracket regex with the current one with epsilon state
-            states[b_end].update({"ε" : prev})
-            b_end = end
-            b_start = b_end
-            b_char = prev
-            # update the current index to start after the solved bracket
-            index = index + len(sub_regex) + 2
-        # in case of special character found
+            prev, start, end = solveBracket(sub_regex, b_end, states)
+            states[b_end].update({"ε": prev})
+            b_end, b_start, b_char = end, end, prev
+            index += len(sub_regex) + 2
         elif regex[index] == '\\':
-            # read the charactr after the \ and make a normal transition from the current state to new one using that character
+            # Handle escaped characters
             index += 1
-            states[b_end].update({regex[index] : (b_end+1)})
-            b_end +=1
-            states.update( { b_end : { "isTerminatingState": False } })
-            b_char = b_start
-            b_start = b_end
-            # increment the regex looping index     
-            index += 1
-        # in case of partitioning found
-        elif regex[index] == '*':
-            # create two states using tompthons rules as described in the slides
             b_end += 1
-            states[b_start].update({" ε " : b_char, "  ε  " : b_end })
-            states[b_char].update({"ε            " : b_end})
-            states.update( { b_end : { "isTerminatingState": False} })
-            b_start = b_end
-            # increment the regex looping index     
+            states[b_end] = {"isFinalState": False}
+            states[b_char].update({regex[index]: b_end})
+            b_char, b_start = b_end, b_end
             index += 1
-        # in case of anding
+        elif regex[index] == '*':
+            # Handle zero or more repetitions
+            b_end += 1
+            states[b_start].update({" ε ": b_char, "  ε  ": b_end})
+            states[b_char].update({"ε            ": b_end})
+            states[b_end] = {"isFinalState": False}
+            b_start = b_end
+            index += 1
         else:
-            # make new state and new transaction from the current state to the new one using that char in the regex
-            b_end +=1
-            states[b_start].update( { regex[index] : b_end })
-            states.update( { b_end : { "isTerminatingState": False } })
-            b_char = b_start
-            b_start = b_end    
-            # increment the regex looping index     
-            index += 1          
-    # return information about the states added by this function when it's called     
+            # Handle regular characters
+            b_end += 1
+            states[b_start].update({regex[index]: b_end})
+            states[b_end] = {"isFinalState": False}
+            b_char, b_start = b_start, b_end
+            index += 1
+
     return b_prev, b_start, b_end
 
+def ORing(index, regex, states, end_state):
+    # Initialize variables for the OR operation
+    ORing_start = ORing_prev = ORing_prev_char = ORing_end = end_state + 1
+    states[ORing_end] = {"isFinalState": False}
 
-def oring(index , regex, states , end_state):
-    """
-        This function used to solve the regex after or operation and save the new states to the current available states
-    """
-
-    # initialization for the new states to be created must start with the last created state + 1
-    oring_start = end_state + 1
-    oring_prev = end_state + 1
-    oring_prev_char = end_state + 1
-    oring_end = end_state + 1
-    # create new state to indicate we are working with bracket regex
-    states.update( { oring_end : { "isTerminatingState": False } })
-
-    # loop over the regex
-    while(index < len(regex)):
-        # in case of oring so the regex after oring operation was solved so return its states
-        if regex[index] == '|' or regex[index] == '+':
-            return index, oring_prev, oring_start, oring_end
-        # in case of special character found
+    while index < len(regex):
+        if regex[index] in {'|', '+'}:
+            # Return when encountering another OR operation
+            return index, ORing_prev, ORing_start, ORing_end
         elif regex[index] == '\\':
-            # take the next character after \ 
+            # Handle escaped characters within OR operation
             index += 1
-            oring_end +=1
-            # make a transition from the current state to new one using that character
-            states[oring_start].update({regex[index] : (oring_end)})
-            states.update( { oring_end : { "isTerminatingState": False } })
-            # update states indeces
-            oring_prev_char = oring_start
-            oring_start = oring_end
-        # in case of bracket found
+            ORing_end += 1
+            states[ORing_start].update({regex[index]: ORing_end})
+            states[ORing_end] = {"isFinalState": False}
+            ORing_prev_char, ORing_start = ORing_start, ORing_end
+            index += 1
         elif regex[index] == '(':
-            # get the regex inside that bracket
-            open_brackets = 1
-            closed_brackets = 0
-            sub_regex = ""
-            j = index+1
-            while(j<len(regex)):
+            # Handle nested brackets within OR operation
+            j = index + 1
+            open_brackets, closed_brackets, sub_regex = 1, 0, ""
+            while j < len(regex):
                 if regex[j] == '(':
-                    open_brackets +=1
+                    open_brackets += 1
                 elif regex[j] == ')':
-                    closed_brackets +=1
-                if (open_brackets == closed_brackets):
+                    closed_brackets += 1
+                if open_brackets == closed_brackets:
                     break
                 sub_regex += regex[j]
                 j += 1
-            # solve that regex using the bracket solver function
-            prev , start, end = solveBracket(sub_regex, oring_end, states) 
-            # connect the resulting states with the current state using one spsilon move
-            states[oring_end].update({"ε" : prev})
-            # update the states indeces
-            oring_end = end
-            oring_start = oring_end
-            oring_prev_char = prev
-            # continue looping over the regex after that bracket
-            index = index + len(sub_regex) + 1
-        # in case of repition found
+            prev, start, end = solveBracket(sub_regex, ORing_end, states)
+            states[ORing_end].update({"ε": prev})
+            ORing_end, ORing_start, ORing_prev_char = end, end, prev
+            index += len(sub_regex) + 1
         elif regex[index] == '*':
-            # create two state and connect between them using tompthon rule as decribed in the slides
-            oring_end += 1
-            states[oring_start].update({"   ε   " : oring_prev_char, "     ε     " : oring_end })
-            states[oring_prev_char].update({"ε        " : oring_end})
-            states.update( { oring_end : { "isTerminatingState": False} })
-            oring_start = oring_end
-        # in case of anding 
+            # Handle zero or more repetitions within OR operation
+            ORing_end += 1
+            states[ORing_start].update({"   ε   ": ORing_prev_char, "     ε     ": ORing_end})
+            states[ORing_prev_char].update({"ε        ": ORing_end})
+            states[ORing_end] = {"isFinalState": False}
+            ORing_start = ORing_end
         else:
-            # make a transition from the current state to new state using that input
-            oring_end +=1
-            states[oring_start].update( { regex[index] : oring_end })
-            states.update( { oring_end : { "isTerminatingState": False } })
-            oring_prev_char = oring_start
-            oring_start = oring_end   
-        # update the regex counter to continue looping over it      
-        index += 1        
-    # return information about the states added by this function when it's called     
-    return len(regex), oring_prev, oring_start, oring_end
+            # Handle regular characters within OR operation
+            ORing_end += 1
+            states[ORing_start].update({regex[index]: ORing_end})
+            states[ORing_end] = {"isFinalState": False}
+            ORing_prev_char, ORing_start = ORing_start, ORing_end
+        index += 1
 
+    return len(regex), ORing_prev, ORing_start, ORing_end
 
 def transform(regex):
-    # create initial state
-    states = { 0 : { "isTerminatingState": False } }
-    #the state and end state to any expression in the regex
-    start_state = 0
-    end_state   = 0
-    prev_char = 0
-    prev_start  = 0
+    # Initialize states and variables for the NFA transformation
+    states = {0: {"isFinalState": False}}
+    start_state = end_state = prev_char = prev_start = 0
     n = len(regex)
     i = 0
+
     while i < n:
-        # in case of special character found
-        if regex[i] == '\\' :
-            # take the next character after \ 
+        if regex[i] == '\\':
+            # Handle escaped characters
             i += 1
-            # make a transition from the current state to new one using that character
             end_state += 1
-            states[start_state].update({regex[i] : end_state})
-            states.update( { end_state : { "isTerminatingState": False } })
-            prev_char = start_state
-            start_state = end_state
-            # update the regex counter to continue looping over it  
-            i +=1
-        # in case of open bracket solve the bracket in another function and update i and states in that function 
+            states[start_state].update({regex[i]: end_state})
+            states[end_state] = {"isFinalState": False}
+            prev_char, start_state = start_state, end_state
+            i += 1
         elif regex[i] == '(':
-            # get the regex inside that bracket
-            open_brackets = 1
-            closed_brackets = 0
-            sub_regex = ""
-            j = i+1
-            while(j<n):
+            # Handle nested brackets
+            j = i + 1
+            open_brackets, closed_brackets, sub_regex = 1, 0, ""
+            while j < n:
                 if regex[j] == '(':
-                    open_brackets +=1
+                    open_brackets += 1
                 elif regex[j] == ')':
-                    closed_brackets +=1
-                if (open_brackets == closed_brackets):
+                    closed_brackets += 1
+                if open_brackets == closed_brackets:
                     break
                 sub_regex += regex[j]
                 j += 1
-            # solve that regex using the bracket solver function
-            prev , start, end = solveBracket(sub_regex, end_state, states) 
-            # connect the resulting states with the current state using one spsilon move
-            states[end_state].update({"ε" : prev})
-            # update the states indeces
-            end_state = end
-            start_state = end_state
-            prev_char = prev
-            # continue looping over the regex after that bracket
+            prev, start, end = solveBracket(sub_regex, end_state, states)
+            states[end_state].update({"ε": prev})
+            end_state, start_state, prev_char = end, end, prev
             i = i + len(sub_regex) + 2
-        # in case of oring 
-        elif regex[i] == '|' or regex[i] == '+':
-            # solve it using oring function (function description above) 
-            i, prev, start,end = oring(i+1, regex, states , end_state)
-            # create new 2 states to connect the oring branches
-            states.update( { end+1 : { "isTerminatingState": False, "     ε     " : prev_start , "      ε       " : prev } })
-            states.update( { end+2 : { "isTerminatingState": False} })
-            states[end].update({"ε" : end+2})
-            states[end_state].update({"ε" : end+2})
-            #update the state indices 
-            prev_char = end + 1
-            end_state = end +2
-            start_state = end_state
-            prev_start = end + 1
-        # in case of repetition
+        elif regex[i] in {'|', '+'}:
+            # Handle OR operations
+            i, prev, start, end = ORing(i + 1, regex, states, end_state)
+            # Update states based on the OR operation
+            states.update({end + 1: {"isFinalState": False, "     ε     ": prev_start, "      ε       ": prev},
+                           end + 2: {"isFinalState": False}})
+            states[end].update({"ε": end + 2})
+            states[end_state].update({"ε": end + 2})
+            prev_char, end_state, start_state, prev_start = end + 1, end + 2, end + 2, end + 1
         elif regex[i] == '*':
-            # create two state and connect between them using tompthon rule as decribed in the slides
+            # Handle zero or more repetitions
             end_state += 1
-            states[start_state].update({"ε   " : prev_char, "ε    " : end_state })
-            states[prev_char].update({"ε     " : end_state})
-            states.update( { end_state : { "isTerminatingState": False} })
+            states[start_state].update({"ε     ": prev_char, "ε    ": end_state})
+            states[prev_start].update({"ε     ": end_state})
+            states[end_state] = {"isFinalState": False}
             start_state = end_state
-            # update the regex counter to continue looping over it  
             i += 1
-        # in case of anding
         else:
-            # make a transition from the current state to new state using that input
+            # Handle regular characters
             end_state += 1
-            states[start_state].update({regex[i] : end_state})
-            states.update( { end_state : { "isTerminatingState": False } })
-            prev_char = start_state
-            start_state = end_state
-            # update the regex counter to continue looping over it  
+            states[start_state].update({regex[i]: end_state})
+            states[end_state] = {"isFinalState": False}
+            prev_char, start_state = start_state, end_state
             i += 1
 
-    # make the last state as out state
-    states[end_state]["isTerminatingState"] = True
-    # sort the state ascending
+    # Set the final state as accepting
+    states[end_state]["isFinalState"] = True
+    # Order the states for consistent output
     states = collections.OrderedDict(sorted(states.items()))
-    # printing the states for debugging
-    #for k, v in states.items(): print(k, v)
 
-    # loop over sorted states and save them as the given example to json file
-    # return the json file content to be displayed in graph format
-    results = {}
-    results.update({"startingState" : ("S" + str(prev_start))})
-    for key , value in states.items():
-        entry = {}
-        for k,v in value.items():
-            if k == "isTerminatingState":
-                entry.update({k : v})
-            else:
-               entry.update(({k : ("S" + str(v))})) 
-        results.update({("S"+ str(key)) : entry})
-    with open('out/nfa.json', 'w') as fp:
+    # Prepare results for JSON output
+    results = {"startingState": f"S{prev_start}"}
+    for key, value in states.items():
+        entry = {k: f"S{v}" if k != "isFinalState" else v for k, v in value.items()}
+        results[f"S{key}"] = entry
+
+    # Write results to a JSON file
+    with open('out/nfa.json', 'w', encoding='utf-8') as fp:
         json.dump(results, fp, ensure_ascii=False)
     return results
